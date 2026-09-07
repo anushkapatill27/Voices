7457ae0f2f7a42b5699ba79540a6ac09133e72bc
anushka <anushka.m.patil06@gmail.com>
2026-09-07 19:30:55 +0530

    4 commit

--- main.py
+++ main.py
@@ -9,28 +9,12 @@ from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Text, ForeignKey
 from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
-# Import your forms from the forms.py
 from forms import CreatePostForm
 from forms import  RegisterForm
 from forms import LoginForm
 from forms import CommentForm
 import os
 
-
-
-
-'''
-Make sure the required packages are installed: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from the requirements.txt for this project.
-'''
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
@@ -46,7 +30,7 @@ gravatar = Gravatar(app,
                     use_ssl=False,
                     base_url=None)
 
-# TODO: Configure Flask-Login
+
 login_manager = LoginManager()
 login_manager.init_app(app)
 
@@ -116,7 +100,7 @@ class Comment(db.Model):
     post_id: Mapped[int] = mapped_column(Integer,db.ForeignKey("blog_posts.id"))
     parent_post = relationship("BlogPost",back_populates="comments")
 
-# TODO: Create a User table for all your registered users. 
+
 class User(UserMixin,db.Model):
     __tablename__ = "Users"
     id: Mapped[int] = mapped_column(Integer,primary_key=True)
@@ -133,7 +117,7 @@ with app.app_context():
     db.create_all()
 
 
-# TODO: Use Werkzeug to hash the user's password when creating a new user.
+
 @app.route('/register',methods=["GET","POST"])
 def register():
     form = RegisterForm()
@@ -155,7 +139,7 @@ def register():
     return render_template("register.html",form=form,current_user=current_user)
 
 
-# TODO: Retrieve a user from the database based on their email. 
+
 @app.route('/login',methods=["GET","POST"])
 def login():
     form = LoginForm()
@@ -189,7 +173,7 @@ def get_all_posts():
     return render_template("index.html", all_posts=posts,current_user=current_user)
 
 
-# TODO: Allow logged-in users to comment on posts
+
 @app.route("/post/<int:post_id>", methods=["GET", "POST"])
 def show_post(post_id):
     requested_post = db.get_or_404(BlogPost, post_id)
@@ -208,7 +192,6 @@ def show_post(post_id):
     return render_template("post.html", post=requested_post,current_user=current_user,form=comment_form)
 
 
-# TODO: Use a decorator so only an admin user can create a new post
 @app.route("/new-post", methods=["GET", "POST"])
 @login_required
 def add_new_post():
@@ -228,7 +211,6 @@ def add_new_post():
     return render_template("make-post.html", form=form,current_user=current_user)
 
 
-# TODO: Use a decorator so only an admin user can edit a post
 @app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
 @post_owner
 def edit_post(post_id):
@@ -251,7 +233,6 @@ def edit_post(post_id):
     return render_template("make-post.html", form=edit_form, is_edit=True,current_user=current_user)
 
 
-# TODO: Use a decorator so only an admin user can delete a post
 @app.route("/delete/<int:post_id>")
 @post_owner
 def delete_post(post_id):
